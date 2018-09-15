bab6bef3f3f96cf15eff9e9bf425d4758a310bc6
akarsh-shrivastava <akarsh.mails@rediffmail.com>
2018-09-15 16:06:32 +0530

    added image processing part

---
+++ Bg_sub.py
@@ -0,0 +1,106 @@
+import numpy as np
+import cv2
+import math
+from skimage.measure import compare_ssim
+
+class Bg_sub:
+	@staticmethod
+	def bg_sub():
+		cap=cv2.VideoCapture(1)
+		ret=True
+		while ret:
+			ret,cam=cap.read()
+			#yuv=cv2.cvtColor(cam,cv2.COLOR_BGR2YUV)
+			cv2.imshow("yuv",cam)
+			#mask = cv2.inRange(cam, (np.array([97,46,17])), (np.array([117,46,17])))
+			if cv2.waitKey(1)==ord("c"):
+				cv2.imwrite("without_fan.jpg",cam)
+			if cv2.waitKey(1)==ord("f"):
+				cv2.imwrite("with_fan.jpg",cam)
+			if cv2.waitKey(5)==27:
+				break
+		cap.release()
+		cv2.destroyAllWindows()
+
+		image1 = cv2.imread("without_fan.jpg")
+		image2 = cv2.imread("with_fan.jpg")
+		grayA = cv2.cvtColor(image1, cv2.COLOR_BGR2GRAY)
+		grayB = cv2.cvtColor(image2, cv2.COLOR_BGR2GRAY)
+		#cv2.imwrite("grayA.jpg",grayA)
+		#cv2.imwrite("grayB.jpg",grayB)
+		(score, diff) = compare_ssim(grayB, grayA, full=True)
+		diff = (diff * 255).astype("uint8")
+		thresh = cv2.threshold(diff, 0, 255,cv2.THRESH_BINARY_INV | cv2.THRESH_OTSU)[1]
+		blur = cv2.GaussianBlur(thresh,(5,5),1)
+		kernel = np.ones((5,5),np.uint8)
+		erode = cv2.erode(blur,kernel,iterations = 1)
+		dilate = cv2.dilate(erode,kernel,iterations = 1)
+		#cv2.imwrite("diff.jpg",diff)
+		#cv2.imwrite("dilate.jpg",dilate)n
+		img1,contours,hierarchy = cv2.findContours(dilate,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_NONE)
+		for c in contours:
+			if cv2.contourArea(c)>1000:
+				x,y,w,h=cv2.boundingRect(c)
+				print "width", w, "height", h
+				cv2.rectangle(image2,(x,y),(x+w,y+h),[255,0,0],1)
+				cv2.drawContours(image2, c, -1, (0,255,0), 2)
+				if max(w,h)==w:
+					roi1 = dilate[y:(y+h), x:(x+(w/2))]
+					roi2 = dilate[y:(y+h),(x+(w/2)):(x+w)]
+					#cv2.imwrite("ROI1.jpg",roi1)
+					#cv2.imwrite("ROI2.jpg",roi2)
+					im1 = np.asarray(roi1, dtype=np.float)
+					im2 = np.asarray(roi2, dtype=np.float)
+					sum1= im1.sum()
+					sum2=im2.sum()
+					if sum1>sum2:
+						top1=tuple(c[c[:, :, 0].argmin()][0])
+						bottom1=tuple(c[c[:, :, 0].argmax()][0])
+						cv2.circle(image2, top1, 3, (255, 0, 0), -1)
+						cv2.circle(image2, bottom1, 3, (0, 0, 255), -1)
+						print "Top: ",top1
+						print "Bottom: ",bottom1
+					else:
+						top1=tuple(c[c[:, :, 0].argmax()][0])
+						bottom1=tuple(c[c[:, :, 0].argmin()][0])
+						cv2.circle(image2, top1, 3, (255, 0, 0), -1)
+						cv2.circle(image2, bottom1, 3, (0, 0, 255), -1)
+						print "Top: ",top1
+						print "Bottom: ",bottom1
+
+					print "hori"
+				else:
+					roi1=dilate[y:(y+(h/2)),x:x+w]
+					roi2=dilate[(y+(h/2)):y+h,x:x+w]
+					#cv2.imwrite("ROI1.jpg",roi1)
+					#cv2.imwrite("ROI2.jpg",roi2)
+					im1 = np.asarray(roi1, dtype=np.float)
+					im2 = np.asarray(roi2, dtype=np.float)
+					sum1= im1.sum()
+					sum2=im2.sum()
+					if sum1>sum2:
+						top1=tuple(c[c[:, :, 1].argmin()][0])
+						bottom1=tuple(c[c[:, :, 1].argmax()][0])
+						cv2.circle(image2, top1, 3, (255, 0, 0), -1)
+						cv2.circle(image2, bottom1, 3, (0, 0, 255), -1)
+						print "Top: ",top1
+						print "Bottom: ",bottom1
+					else:
+						top1=tuple(c[c[:, :, 1].argmax()][0])
+						bottom1=tuple(c[c[:, :, 1].argmin()][0])
+						cv2.circle(image2, top1, 3, (255, 0, 0), -1)
+						cv2.circle(image2, bottom1, 3, (0, 0, 255), -1)
+						print "Top: ",top1
+						print "Bottom: ",bottom1
+					print "vert"
+
+				if w>65:
+					output=(top1,bottom1,"Tedha")
+				else:
+					output=(top1,bottom1,"Seedha")
+				
+		cv2.imwrite("final.jpg",image2)
+
+		cv2.waitKey(0)
+
+		return output
--- do_motion.py
+++ do_motion.py
@@ -1,19 +1,40 @@
+#########################################################################################################
+# Copyright 2018 SRM Team Humaniod
+# All Rights Reserved
+#
+# Licensed under the ApashyamKiriKiri License, Version 1.13.[insertRandomNumberHere]
+# you may not use this file except in compliance with the License.
+# 
+# This is a proprietry code. No one except the Team Leads, Domain Heads
+# or Coding Domain Senior Balak or Balikas of SRMTH are supposed to read this.
+#
+# If you are not a Team Lead, Domain Head or Coding Domain Senior Balak or Balika
+# your eyes will pop out in the next 5 seconds.
+#
+# No one except the Owners have the right to access or modify the code.
+#
+#########################################################################################################
+
+# Owner     : SRM Team Humaniod, Kattankulathur
+# Author    : Akarsh Shrivastava
+# Maintainer: No one tries to maintain this.
+#
+# Yes I was so jobless that I did all this
+
 from time import sleep
 from Read import Read
 from Dynamixel import Dynamixel
 from Equations import Equations
+from Bg_sub import Bg_sub
 
 '''constrints for m3 & m4- (-132.4, 140.91)'''
 
 
 if __name__=='__main__':
 	dxl=Dynamixel()
-	'''x1,y1=379,173
-	x2,y2=385,447
-
-	x1,y1=Equations.pixel_to_real(x1,y1)
-	x2,y2=Equations.pixel_to_real(x2,y2)'''
-	x1,y1,x2,y2=28,5,5,5
+	
+	detection=Bg_sub.bg_sub()
+	x1,y1,x2,y2=detection[0][0],detection[0][1],detection[1][0],detection[1][1]
 	try:
 		th1,th2,th3=Equations.get_th(x1,y1,x2,y2)
 		if th2>130 or th2<-130 or th3<-130 or th3>130:
@@ -31,3 +52,5 @@ if __name__=='__main__':
 		dxl.set_position(motion[0])
 		sleep(motion[1])
 		#print motion[0],"\t\t\t",motion[1]
+
+	dxl.initial_postion()
