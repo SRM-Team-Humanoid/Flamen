1c4752b0fedf33142c7941d430283f04b0af07ad
akarsh-shrivastava <akarsh.mails@rediffmail.com>
2018-09-22 20:10:10 +0530

    More verbrose now

--- Equations.py
+++ Equations.py
@@ -20,16 +20,28 @@ class Equations:
 	def get_th(x1,y1,x2,y2):
 		l1=9.3
 		l2=7
-
+		print "x1,y1 = ",x1, y1
+		print "x2,y2 = ",x2, y2
 
 		tha=float(atan((y1-y2)/(x1-x2)))
-		x=(x2+2*cos(tha))
-		y=(y2+2*sin(tha))
+		if  tha<0:
+			tha+=pi
+		x=(x2+3*cos(tha))
+		y=(y2+3*sin(tha))
+		print "x,y = ",x,y
 		dist=float(sqrt((x*x)+(y*y)))
+		print "dist = ",dist
 		th2=float(acos(((dist*dist) - (l1*l1) - (l2*l2))/(2*l1*l2)))
+		print "th2 = ",th2
 		d1=float(acos(y/dist))
+		print "d1 = ",d1
 		d2=float(acos((dist**2 + l1**2 - l2**2)/(2*l1*dist)))
+		print "d2 = ",d2
 		th1=(d1-d2)
+		print "th1 = ",th1
+		#if tha<0:
+		#	tha+=pi
+		print "tha = ",tha	
 		th3=pi/2-tha-th1-th2
-		return (degrees(th1),-degrees(th2),-degrees(th3))
+		return (-degrees(th1),-degrees(th2),-degrees(th3))
 		#return (0,-45,-45)
